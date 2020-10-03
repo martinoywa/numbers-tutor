3c362ae723bacd326f08b20b6746e72b26d71acd
Martin Oywa <martinoywa2@gmail.com>
2020-10-03 17:23:15 +0300

    Update links in templates and functions

--- run.py
+++ run.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def base_app():
+@app.route("/")
+def index():
     return render_template("index.html")
+
+
+@app.route("/write")
+def write():
+    return render_template("write.html")
+
+
+@app.route("/say")
+def say():
+    return render_template("say.html")
+
 
 if __name__ == "__main__":
     app.run()
