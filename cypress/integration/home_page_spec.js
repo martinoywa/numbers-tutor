c4ce62e1d4924ffd410c5af8b2759f592d7ba7d0
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-12-09 16:38:53 +0300

    Add tests to check presence of buttons

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,6 +1,13 @@
 describe('HomePage', () => {
     it('successfully loads', () => {
-        cy.visit('/')
+        cy.visit('/index')
     })
-    
+    it('contains the write button', () => {
+        cy.get('.buttons').find('>a')
+        cy.contains('Write')
+    })
+    it('contains the Voice button', () => {
+        cy.get('.buttons').find('>a')
+        cy.contains('Voice')
+    })
 })
