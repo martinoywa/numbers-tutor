2f153f4a34c7e7b683f7d0d9aaf66533c87cc61a
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-12-09 16:36:39 +0300

    Specify the templates folder

--- app/views.py
+++ app/views.py
@@ -1,17 +1,25 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template
 
 
-home = Blueprint("home", __name__,template_folder='templates')
-@home.route("/index")
+home = Blueprint("home", __name__, template_folder='templates')
+
+
+@home.route("/")
 def index():
     return render_template('index.html')
 
-write = Blueprint("write", __name__)
+
+write = Blueprint("write", __name__, template_folder='templates')
+
+
 @write.route("/")
 def index():
-    return "Write Page"
+    return render_template('write.html')
 
-speak = Blueprint("speak", __name__)
+
+speak = Blueprint("speak", __name__, template_folder='templates')
+
+
 @speak.route("/")
 def index():
-    return "Speak Page"
+    return render_template('voice.html')
