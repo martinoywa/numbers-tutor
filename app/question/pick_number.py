c4588dd2e9617132099eea90d8095226df972bf9
Martin Oywa <martinoywa2@gmail.com>
2020-12-11 12:57:22 +0300

    Add question functionality

---
+++ app/question/pick_number.py
@@ -0,0 +1,11 @@
+from random import randint
+
+
+def pick_number():
+    """
+    Incrementally pick a number from 0 to 10.
+    For now, we'll start with random numbers
+    Returns:
+        A digit
+    """
+    return randint(0, 10)
