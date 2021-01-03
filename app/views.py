4c8f7228a0d0cc2967de1408d0a26fe0e70435ec
Martin Oywa <martinoywa2@gmail.com>
2021-01-03 21:57:47 +0300

    Refactor views file

--- app/views.py
+++ app/views.py
@@ -1,17 +1,24 @@
 from flask import Blueprint
 
-
 home = Blueprint("home", __name__)
+
+
 @home.route("/")
 def index():
     return "Home Page"
 
+
 write = Blueprint("write", __name__)
+
+
 @write.route("/")
 def index():
     return "Write Page"
 
+
 speak = Blueprint("speak", __name__)
+
+
 @speak.route("/")
 def index():
     return "Speak Page"
